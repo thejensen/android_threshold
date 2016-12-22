40928e83c01014177dc33d42f7e9a39d6bf6ef29
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-21 22:00:03 -0800

    save title of input to firebase.

---
+++ app/src/main/java/com/epicodus/politicalactivismtracker/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.politicalactivismtracker;
+
+/**
+ * Created by jensese on 12/21/16.
+ */
+
+public class Constants {
+    public static final String FIREBASE_CHILD_ACTION_TITLE = "Title";
+}
--- app/src/main/java/com/epicodus/politicalactivismtracker/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/politicalactivismtracker/ui/MainActivity.java
@@ -9,7 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.epicodus.politicalactivismtracker.Constants;
 import com.epicodus.politicalactivismtracker.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,8 +32,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.inputDescriptionEditText) EditText mInputDescriptionEditText;
     @Bind(R.id.submitNewActionInputButton) Button mSubmitNewActionInputButton;
 
+    private DatabaseReference mActionTitleReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mActionTitleReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_ACTION_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -53,11 +64,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String price = mInputPriceEditText.getText().toString();
             String description = mInputDescriptionEditText.getText().toString();
             Log.v(TAG, "Location is: " + location);
+
+            saveActionTitleToFirebase(title);
             Toast.makeText(MainActivity.this, "OMG WHY", Toast.LENGTH_SHORT).show();
         }
         if (v == mFindActionsButton) {
             Intent intent = new Intent(MainActivity.this, ActivismActivity.class);
             startActivity(intent);
         }
+    }
+
+    public void saveActionTitleToFirebase(String title) {
+        mActionTitleReference.push().setValue(title);
     }
 }
