abb4880dda8126d619fbe1742c342269f40bf321
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-22 02:49:09 -0800

    Inflate menu layout in the main activity.

--- app/src/main/java/com/epicodus/politicalactivismtracker/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/politicalactivismtracker/ui/MainActivity.java
@@ -3,6 +3,8 @@ package com.epicodus.politicalactivismtracker.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -76,5 +78,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(MainActivity.this, ActionListActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
