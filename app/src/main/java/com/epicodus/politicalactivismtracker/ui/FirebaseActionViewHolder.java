d15d2ccac9fe3509ccf96ecca0faa77ccd0b608e
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-21 22:59:07 -0800

    Add Parceler and FirebaseUI dependencies, prep for added actions activity.

--- app/src/main/java/com/epicodus/politicalactivismtracker/ui/FirebaseActionViewHolder.java
+++ app/src/main/java/com/epicodus/politicalactivismtracker/ui/FirebaseActionViewHolder.java
@@ -1,14 +1,25 @@
 package com.epicodus.politicalactivismtracker.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.epicodus.politicalactivismtracker.Constants;
 import com.epicodus.politicalactivismtracker.R;
 import com.epicodus.politicalactivismtracker.models.Action;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
 
 /**
  * Created by jensese on 12/21/16.
@@ -57,6 +68,28 @@ public class FirebaseActionViewHolder extends RecyclerView.ViewHolder implements
 
     @Override
     public void onClick(View v) {
+        final ArrayList<Action> actions = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ACTIONS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
 
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    actions.add(snapshot.getValue(Action.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, SavedActionActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("restaurants", Parcels.wrap(actions));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
