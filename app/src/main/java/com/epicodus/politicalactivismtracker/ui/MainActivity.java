4998dd50d0eeb3e07d6747eefaae8510799a87f7
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-21 20:36:26 -0800

    Fuss more with layout.

--- app/src/main/java/com/epicodus/politicalactivismtracker/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/politicalactivismtracker/ui/MainActivity.java
@@ -1,15 +1,31 @@
 package com.epicodus.politicalactivismtracker.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.epicodus.politicalactivismtracker.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findActionsButton) Button mFindActionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindActionsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, ActivismActivity.class);
+        startActivity(intent);
     }
 }
