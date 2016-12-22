90dba19024d71a49ca71560a29ea09e41be91308
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-21 23:43:04 -0800

    Retrieve action list in a recycler adapter, woo. WIP: individual pages for action items.

--- app/src/main/java/com/epicodus/politicalactivismtracker/ui/AddedActionsActivity.java
+++ app/src/main/java/com/epicodus/politicalactivismtracker/ui/AddedActionsActivity.java
@@ -2,13 +2,18 @@ package com.epicodus.politicalactivismtracker.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.epicodus.politicalactivismtracker.Constants;
 import com.epicodus.politicalactivismtracker.R;
+import com.epicodus.politicalactivismtracker.models.Action;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class AddedActionsActivity extends AppCompatActivity {
     private DatabaseReference mActionReference;
@@ -20,5 +25,27 @@ public class AddedActionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_added_actions);
+        ButterKnife.bind(this);
+
+        mActionReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ACTIONS);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Action, FirebaseActionViewHolder>(Action.class, R.layout.action_list_item, FirebaseActionViewHolder.class, mActionReference) {
+            @Override
+            protected void populateViewHolder(FirebaseActionViewHolder viewHolder, Action model, int position) {
+                viewHolder.bindAction(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
